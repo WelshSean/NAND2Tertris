83148a3b0dc60d465c8b6a8d1c4ff8726f36275d
Sean Clarke <sean@nay-clarke.com>
2017-05-29 20:09:20 +0100

    Have a version working but the screen is flashy - need to fix logic

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -14,39 +14,73 @@
 // Put your code here.
 
 // fill - will be 0 if we arent writing anything or -1 if we want to write 16 bits of 1
-	@8191
+	@8192
 	D=A
 	@num
 	M=D 		// n=8191 - number of 16-bit words to blank the screen
 
 
+
+
+(MAININFINITE)
 // spoint - screen pointer - used to iterate over the memory locations that represent the screen
 // 							 Screen is 512 x 256, each row is 32 consecutive 16-bit words
 //							 ie we will need to write 256 x 32 16-bit words
 //							  we initialise to the base address
+
 	@SCREEN
 	D=A
 	@spoint
 	M=D
 
-(MAININFINITE)
-	@KBD
-	D=M
-	@KEYPRESSED
-	D;JNE 
+// First of alll blank the screen just in case we'd filled it!
 
-// Note that if we didnt JUMP them we get here which means that no key is pressed
-	@MAININFINITE
-	0;JMP
-
-
-(KEYPRESSED)
 	@1		//initialise counter variable
 	D=A
 	@n
 	M=D
-(SCREENLOOP)
-	@spoint		
+
+(BLANKLOOP)
+	@spoint	
+	A=M 			// Load pointer into address register
+	M=0				// Write 0 (16 blanks) into the relevant place in the Screen memory map
+	@spoint
+	M=M+1			// increment the pointer address
+
+	@n  			// increment the counter
+	M=M+1
+
+	@num
+	D=M
+	@n
+	D=D-M			// Carry on looping if current index - number of 16-bit words needed > 0
+	@BLANKLOOP
+	D;JGT
+
+
+	@KBD					// Check if a key is pressed and jump out of maininifite if true
+	D=M
+	@KEYPRESSED
+	D;JNE 
+
+// Note that if we didnt JUMP them we get here which means that no key is pressed - carry on looping inifitely
+	@MAININFINITE			
+	0;JMP
+
+
+(KEYPRESSED)
+	@SCREEN // Initialise screen pointer
+	D=A
+	@spoint
+	M=D
+	
+	@1		//initialise counter variable
+	D=A
+	@n
+	M=D
+
+(FILLLOOP)
+	@spoint	
 	A=M 			// Load pointer into address register
 	M=-1			// Write -1 (16 pixels) into the relevant place in the Screen memory map
 	@spoint
@@ -59,7 +93,7 @@
 	D=M
 	@n
 	D=D-M			// Carry on looping if current index - number of 16-bit words needed > 0
-	@SCREENLOOP
+	@FILLLOOP
 	D;JGT
 
 
