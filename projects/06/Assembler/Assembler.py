d19e3e2b5763a5a3e873a14a146c4b31c84d14ee
Sean Clarke <sean@nay-clarke.com>
2017-06-11 17:37:07 +0100

    Starting to build Parser Object

--- projects/06/Assembler/Assembler.py
+++ projects/06/Assembler/Assembler.py
@@ -11,22 +11,36 @@ class Parser(object):
         """ Open the file specified and ready it for parsing """
 
         stem = fileName.split(".")[0]  # Making assumption that file is stem.asm
-        print "Stem: " + str(stem)
+        self.fileIndex=0
         self.inputFile = open(fileName, 'r')
+        with open(fileName) as f:
+            self.lines = f.read().splitlines()
+        self.lines = self.removeCommentsAndWhitespace(self.lines)
         self.outputFile = open(stem + ".hack", 'w')
-        #self.line =  self.inputFile.readline()    # Current line in program
-        self.advance()
 
-    def getCommand(self):
-        """ return the raw value of the curret line - used for testing """
-        return self.line
+    @staticmethod
+    def removeCommentsAndWhitespace(fileList):
+        """ when passed a list, this function will remove all entries that are empty lines, whitespace or comments"""
+        import re
+        return list(filter(lambda x: (not re.match('^ *$', x)and not re.match('^\/\/.*$', x )) , fileList))
+
+    def getLine(self, index):
+        """ return the raw value of an indexed line - index = line number - 1 - used for testing """
+        return self.lines[index]
+
+    def getNumberLines(self):
+        """ return the number of lines in file dict - used for testing"""
+        return len(self.lines)
 
     def advance(self):
         """ move to the next line that doesnt line containing a command """
         import re
         while True:
+            print "Searching...."
             self.line = self.inputFile.readline().rstrip("\r\n")
-            if (not re.match(self.line, '^\/\/.*$')) and (not re.match(self.line, '^$')):
+            print self.line
+            if (not re.match('^\/\/.*$', self.line ) and not re.match('^$', self.line)):
+                print "DIDNT MATCH"
                 break
 
     def commandType(self):
--- projects/06/Assembler/testAssembler.py
+++ projects/06/Assembler/testAssembler.py
@@ -5,17 +5,25 @@ from Assembler import Parser
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.testParser = Parser('/Users/Sean/Desktop/nand2tetris/projects/06/add/Add.asm')
+
+    def test_removeCommentsAndWhitespace(self):
+        test_list=['//ssss', '      ', 'one', 'two', '//three', 'three']
+        output=Parser.removeCommentsAndWhitespace(test_list)
+        self.assertEqual(output, ['one', 'two', 'three'])
+
     def test_init(self):
         """ Parser setup should read the first line of the file"""
-        #testParser = Parser('/Users/Sean/Desktop/nand2tetris/projects/06/add/Add.asm')
-        testLine = self.testParser.getCommand()
-        self.assertEqual(testLine, "@2")
+        testParser = Parser('/Users/Sean/Desktop/nand2tetris/projects/06/add/Add.asm')
+        self.assertEqual(self.testParser.getLine(0), "@2")
+        self.assertEqual(self.testParser.getLine(3), "D=D+A")
+        self.assertEqual(testParser.getNumberLines(), 6)
 
-    def test_advance(self):
-        """ Test that advance reads the next line of the file"""
-        self.testParser.advance()
-        testLine = self.testParser.getCommand()
-        self.assertEqual(testLine, "D=A")
+    # def test_advance(self):
+    #     """ Test that advance reads the next line of the file"""
+    #     self.testParser.advance()
+    #     testLine = self.testParser.getCommand()
+    #     print "TESTLINE: " + str(testLine)
+    #     self.assertEqual(testLine, "D=A")
 
     if __name__ == '__main__':
         unittest.main()
