e8b662b1ab1445b96f4c1fed643885d4713e24e4
Sean Clarke <sean@nay-clarke.com>
2017-07-04 23:17:30 +0100

    All my tests are passing - try their tests tomorrow

--- projects/07/VMTranslator/testCodeWriter.py
+++ projects/07/VMTranslator/testCodeWriter.py
@@ -19,22 +19,129 @@ class MyTestCaseCodeWriter(unittest.TestCase):
         counter = 0
         with open('/tmp/testfile', mode='r') as f:
             lines = f.read().splitlines()
-            print lines
         for line in lines:
             print line
             self.assertEqual(line, answer[counter])
             counter +=1
 
     def test_writeArithmeticAdd(self):
-        answer = ['one', 'two', 'three']
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=D+*SP',
+                  'M=D+M', '@SP\t\t//SP++', 'M=M+1' ]
         self.testCodeWriter.writeArithmetic('add')
-        for line in answer:
-            pass
-            #self.assertEqual(self.reader.readline().strip(), line)
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
 
 
+    def test_writeArithmeticSub(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=D+*SP',
+                    'M=D-M', '@SP\t\t//SP++', 'M=M+1']
+        self.testCodeWriter.writeArithmetic('sub')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
 
+    def test_writeArithmeticOr(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=D+*SP',
+                    'M=D|M', '@SP\t\t//SP++', 'M=M+1']
+        self.testCodeWriter.writeArithmetic('or')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
 
+    def test_writeArithmeticAnd(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=D+*SP',
+                    'M=D&M', '@SP\t\t//SP++', 'M=M+1']
+        self.testCodeWriter.writeArithmetic('and')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
+
+    def test_writeArithmeticNeg(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=-*SP', 'M=-M', '@SP\t\t//SP++', 'M=M+1']
+        self.testCodeWriter.writeArithmetic('neg')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
+
+    def test_writeArithmeticNot(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//*SP=-*SP', 'M=!M', '@SP\t\t//SP++', 'M=M+1']
+        self.testCodeWriter.writeArithmetic('not')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
+
+    def test_writeArithmeticGT(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M',
+                  '@GREATER\t// Jump to greater if top item in stack is greater than the one below it', 'D-M;JGT',
+                  'M=0\t// Not greater therefore set stack entry at SP to 0 and then jump to end infinite loop',
+                  '@SP\t// Increment SP', 'M=M+1', '@INFLOOP', '0;JMP', '(GREATER)',
+                  'M=1\t// We jumped here because top item was greate than item below it so set top Stack entry to 1',
+                  '@SP\t// Increment SP', 'M=M+1', '(INFLOOP)', '@INFLOOP', '0;JMP']
+        self.testCodeWriter.writeArithmetic('gt')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
+
+    def test_writeArithmeticLT(self):
+        answer = ['@SP\t\t//SP--', 'M=M-1', 'A=M\t\t//D=*SP', 'D=M', '@SP\t\t//SP--', 'M=M-1', 'A=M',
+                  '@GREATER\t// Jump to greater if top item in stack is greater than the one below it', 'D-M;JGT',
+                  'M=1\t// Not greater therefore set stack entry at SP to 1 and then jump to end infinite loop',
+                  '@SP\t// Increment SP', 'M=M+1', '@INFLOOP', '0;JMP', '(GREATER)',
+                  'M=0\t// We jumped here because top item was greate than item below it so set top Stack entry to 0',
+                  '@SP\t// Increment SP', 'M=M+1', '(INFLOOP)', '@INFLOOP', '0;JMP']
+        self.testCodeWriter.writeArithmetic('lt')
+        self.testCodeWriter.close()
+        counter = 0
+        with open('/tmp/testfile', mode='r') as f:
+            lines = f.read().splitlines()
+            self.assertNotEqual(len(lines), 0)
+        for line in lines:
+            print line
+            self.assertEqual(line, answer[counter])
+            counter += 1
 
 
 if __name__ == '__main__':
